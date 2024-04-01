ca31ede3d8a2bae4123dfa510daa93e1651af6c0
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-01 16:04:21 +0700

    menuliskan for pada output

--- P6.cpp
+++ P6.cpp
@@ -31,6 +31,7 @@ int main(){
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
     }
+    for(int i=0;i<3;i++){
     cout << endl;
     cout << "\n NIM : " << mhs.nim;
     cout << "\n Nama : " << mhs.nama;
@@ -38,4 +39,5 @@ int main(){
     cout << "\n \t Desa : " << mhs.alamat.desa;
     cout << "\n \t Kota : " << mhs.alamat.kota;
     cout << "\n Umur : " << mhs.umur;
+    }
 }
